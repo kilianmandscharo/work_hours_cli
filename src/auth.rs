c0c9c60b68768739270cabc8bd0e11043bf720f6
Kilian <kilianmandscharo@gmail.com>
2023-07-01 21:17:18 +0200

    Implement authorizer struct

---
+++ src/auth.rs
@@ -0,0 +1,211 @@
+use core::fmt;
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
+use std::{string::FromUtf8Error, time::Duration};
+
+const TOKEN_DURATION: Duration = Duration::from_secs(10 * 60);
+const FILE_NAME: &str = "token.json";
+const LOGIN_URL: &str = "http://localhost:8080/login";
+const REFRESH_URL: &str = "http://localhost:8080/refresh";
+
+#[derive(Debug)]
+pub enum AuthError {
+    JSONError(serde_json::Error),
+    HTTPError(reqwest::Error),
+    FSError(std::io::Error),
+    UTF8Error(FromUtf8Error),
+    AuthError(u16),
+}
+
+impl std::fmt::Display for AuthError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AuthError::JSONError(err) => write!(f, "JSON error: {}", err),
+            AuthError::HTTPError(err) => write!(f, "HTTP error: {}", err),
+            AuthError::FSError(err) => write!(f, "File system error: {}", err),
+            AuthError::UTF8Error(err) => write!(f, "Byte parsing error: {}", err),
+            AuthError::AuthError(status) => write!(f, "Authentification error: {}", status),
+        }
+    }
+}
+
+impl From<serde_json::Error> for AuthError {
+    fn from(error: serde_json::Error) -> Self {
+        AuthError::JSONError(error)
+    }
+}
+
+impl From<reqwest::Error> for AuthError {
+    fn from(error: reqwest::Error) -> Self {
+        AuthError::HTTPError(error)
+    }
+}
+
+impl From<std::io::Error> for AuthError {
+    fn from(error: std::io::Error) -> Self {
+        AuthError::FSError(error)
+    }
+}
+
+impl From<FromUtf8Error> for AuthError {
+    fn from(error: FromUtf8Error) -> Self {
+        AuthError::UTF8Error(error)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Token {
+    jwt: String,
+    expires_at: std::time::SystemTime,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Login {
+    email: String,
+    password: String,
+}
+
+pub struct Authorizer {
+    token: Arc<Mutex<Option<Token>>>,
+}
+
+impl Authorizer {
+    pub fn new() -> Authorizer {
+        let token: Option<Token> = match load_token_from_file() {
+            Ok(token) => Some(token),
+            _ => None,
+        };
+        Authorizer {
+            token: Arc::new(Mutex::new(token)),
+        }
+    }
+
+    pub fn login(&mut self, email: String, password: String) -> Result<(), AuthError> {
+        if !self.login_necessary() {
+            return Ok(());
+        }
+
+        let login = Login { email, password };
+        let login = serde_json::to_string(&login)?;
+
+        let client = reqwest::blocking::Client::new();
+        let res = client.post(LOGIN_URL).body(login).send()?;
+
+        let status = res.status();
+        if !res.status().is_success() {
+            return Err(AuthError::AuthError(status.as_u16()));
+        }
+
+        let token = res.text()?;
+        let token = Token::new(String::from(token));
+
+        save_token_to_file(&token)?;
+        *self.token.lock().unwrap() = Some(token);
+
+        Ok(())
+    }
+
+    fn refresh_token(&mut self, token: &Token) -> Result<(), AuthError> {
+        if !self.login_necessary() {
+            return Ok(());
+        }
+
+        let client = reqwest::blocking::Client::new();
+        let res = client
+            .post(REFRESH_URL)
+            .header("Authorization", format!("Bearer {}", token.jwt))
+            .send()?;
+
+        let status = res.status();
+        if !res.status().is_success() {
+            return Err(AuthError::AuthError(status.as_u16()));
+        }
+
+        let token = res.text()?;
+        let token = Token::new(String::from(token));
+
+        save_token_to_file(&token)?;
+        *self.token.lock().unwrap() = Some(token);
+
+        Ok(())
+    }
+
+    fn login_necessary(&self) -> bool {
+        if let Some(ref token) = *self.token.lock().unwrap() {
+            if token.has_expired() {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            true
+        }
+    }
+
+    // pub fn start_refresh_interval(&mut self) {
+    //     let token = Arc::clone(&self.token);
+    //
+    //     std::thread::spawn(move || loop {
+    //         println!("Checking token for refresh");
+    //
+    //         let now = std::time::SystemTime::now();
+    //
+    //         if let Some(token) = token.lock().unwrap().clone() {
+    //             if let Ok(expiry_time) = token.expires_at.duration_since(now) {
+    //                 if expiry_time < Duration::from_secs(30) {
+    //                     self.refresh_token(&token);
+    //                 }
+    //             }
+    //         }
+    //
+    //         std::thread::sleep(Duration::from_secs(2));
+    //     });
+    // }
+}
+
+impl Token {
+    fn new(jwt: String) -> Token {
+        let expires_at = std::time::SystemTime::now() + TOKEN_DURATION;
+        Token { jwt, expires_at }
+    }
+
+    fn has_expired(&self) -> bool {
+        let now = std::time::SystemTime::now();
+        if let Ok(_) = self.expires_at.duration_since(now) {
+            false
+        } else {
+            true
+        }
+    }
+}
+
+// impl Clone for Token {
+//     fn clone(&self) -> Self {
+//         Token {
+//             jwt: self.jwt.clone(),
+//             expires_at: self.expires_at.clone(),
+//         }
+//     }
+// }
+
+fn save_token_to_file(token: &Token) -> Result<(), std::io::Error> {
+    let token = serde_json::to_string(&token)?;
+    std::fs::write(FILE_NAME, token)?;
+    Ok(())
+}
+
+fn load_token_from_file() -> Result<Token, AuthError> {
+    let token = String::from_utf8(std::fs::read(FILE_NAME)?)?;
+    let token = serde_json::from_str(&token)?;
+    Ok(token)
+}
+
+#[test]
+fn token_test() {
+    let jwt = String::from("test_token_content");
+    let token = Token::new(jwt);
+    let _ = save_token_to_file(&token).unwrap();
+    let token = load_token_from_file().unwrap();
+    assert_eq!("test_token_content", token.jwt);
+    assert_eq!(false, token.has_expired());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use chrono::{Duration, Utc};
+// use chrono::{Duration, Utc};
 use serde::{Deserialize, Serialize};
-use std::path::Path;
+// use std::path::Path;
+
+mod auth;
 
 #[derive(Serialize, Deserialize)]
 struct BlockCreate {
@@ -16,17 +18,11 @@ struct PauseCreate {
 }
 
 #[derive(Serialize, Deserialize)]
-struct Login {
-    email: String,
-    password: String,
-}
-
-#[derive(Serialize, Deserialize)]
 struct TokenResponse {
     token: String,
 }
 
-const SERVER_URL: &str = "http://localhost:8080";
+// const SERVER_URL: &str = "http://localhost:8080";
 
 fn main() {
     // loop {
@@ -46,8 +42,8 @@ fn main() {
     //     }
     // }
     // add_block().expect("could not parse struct to json");
-    let token = login().unwrap();
-    add_block(token).unwrap();
+    // let token = login().unwrap();
+    // add_block(token).unwrap();
 }
 
 // fn get_blocks() {
@@ -55,71 +51,52 @@ fn main() {
 // }
 //
 
-fn login() -> Result<String, serde_json::Error> {
-    dotenv::dotenv().unwrap();
-
-    let email = std::env::var("EMAIL").unwrap();
-    let password = std::env::var("PASSWORD").unwrap();
-
-    let login = Login { email, password };
-    let login = serde_json::to_string(&login)?;
-
-    let client = reqwest::blocking::Client::new();
-    let path = Path::new(SERVER_URL).join("login");
-    let path = path.to_str().unwrap();
-
-    let res = client.post(path).body(login).send().unwrap();
-    let token = res.text().unwrap();
-
-    Ok(token)
-}
-
-fn now_with_offset(offset: i64) -> String {
-    let with_offset = Utc::now() - Duration::hours(offset);
-    with_offset.to_rfc3339()
-}
-
-fn test_block_json() -> serde_json::Result<String> {
-    let block_start = now_with_offset(10);
-    let block_end = now_with_offset(1);
-    let pause_start = now_with_offset(6);
-    let pause_end = now_with_offset(5);
-
-    let pause = PauseCreate {
-        start: pause_start,
-        end: pause_end,
-    };
-
-    let block = BlockCreate {
-        start: block_start,
-        end: block_end,
-        pauses: vec![pause],
-    };
-
-    let block = serde_json::to_string(&block)?;
-
-    Ok(block)
-}
-
-fn add_block(token: String) -> Result<(), ()> {
-    let block = test_block_json().unwrap();
-
-    let client = reqwest::blocking::Client::new();
-
-    let path = Path::new(SERVER_URL).join("block");
-    let path = path.to_str().unwrap();
-
-    let res = client
-        .post(path)
-        .header("Authorization", format!("Bearer {}", &token[10..]))
-        .body(block)
-        .send()
-        .unwrap();
-
-    println!("{}", res.status());
-
-    Ok(())
-}
+// fn now_with_offset(offset: i64) -> String {
+//     let with_offset = Utc::now() - Duration::hours(offset);
+//     with_offset.to_rfc3339()
+// }
+//
+// fn test_block_json() -> serde_json::Result<String> {
+//     let block_start = now_with_offset(10);
+//     let block_end = now_with_offset(1);
+//     let pause_start = now_with_offset(6);
+//     let pause_end = now_with_offset(5);
+//
+//     let pause = PauseCreate {
+//         start: pause_start,
+//         end: pause_end,
+//     };
+//
+//     let block = BlockCreate {
+//         start: block_start,
+//         end: block_end,
+//         pauses: vec![pause],
+//     };
+//
+//     let block = serde_json::to_string(&block)?;
+//
+//     Ok(block)
+// }
+//
+// fn add_block(token: String) -> Result<(), ()> {
+//     let block = test_block_json().unwrap();
+//
+//     let client = reqwest::blocking::Client::new();
+//
+//     let path = Path::new(SERVER_URL).join("block");
+//     let path = path.to_str().unwrap();
+//
+//     let res = client
+//         .post(path)
+//         .header("Authorization", format!("Bearer {}", token))
+//         .body(block)
+//         .send()
+//         .unwrap();
+//
+//     println!("{}", res.status());
+//
+//     Ok(())
+// }
 
 // struct Block {
 //     id: i32,
